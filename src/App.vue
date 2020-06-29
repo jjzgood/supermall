41b57c568385d3bf85fd7183570b4087049b9d58
jjzgood <jjzgood@126.com>
2020-06-29 23:09:19 +0800

    add tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view />
+    <main-tab-bar />
+  </div>
 </template>
 
 <script>
+// import MainTabBar from "components/content/mainTabbar/MainTabBar";
+import MainTabBar from "../src/components/content/mainTabbar/MainTabBar";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    MainTabBar
+  }
 };
 </script>
 
 <style>
+@import "assets/css/base.css";
 </style>
+
